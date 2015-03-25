09487de2dab0104a87b557879abf84bf889301f4
LionelGies <lionelgies0119@gmail.com>
2015-03-25 08:14:49 +0100

    html_details create table Delete

--- db/migrate/20120120105349_create_html_details.rb
+++
@@ -1,9 +0,0 @@
-class CreateHtmlDetails < ActiveRecord::Migration
-  def change
-    create_table :html_details do |t|
-      t.references :block, :null => false
-      t.text :contents, :null => false, :limit => 64.kilobytes + 1
-      t.timestamps
-    end
-  end
-end
