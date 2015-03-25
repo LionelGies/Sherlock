89d1838a221bdc965f98d2f3ecfe81abe8c7adc5
LionelGies <lionelgies0119@gmail.com>
2015-03-25 08:35:08 +0100

    Heorku Delopy

--- db/migrate/20120616093224_create_data_log_details.rb
+++ db/migrate/20120616093224_create_data_log_details.rb
@@ -4,7 +4,7 @@ class CreateDataLogDetails < ActiveRecord::Migration
       t.references  :block, :null => false
       t.string      :day
       t.string      :hour
-      t.string      :location
+      # t.string      :location
       t.text        :contents, :null => false, :limit => 64.kilobytes + 1      
       t.timestamps      
     end    
